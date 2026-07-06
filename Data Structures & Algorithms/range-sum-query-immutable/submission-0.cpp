ea24c1d007e0fa5c73e94dfa5e5978a7a81a1443
Ans Christy <anschristykl@gmail.com>
2026-07-06 23:03:17 +0530

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.cpp
@@ -0,0 +1,29 @@
+class NumArray {
+private:
+    vector<int> nums;
+    vector<int> prefix;
+
+public:
+    NumArray(vector<int>& nums) {
+        this->nums = nums;
+        prefix.resize((nums.size()));
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            prefix[i] = nums[i] + prefix[i - 1];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if (left == 0) {
+            return prefix[right];
+        }
+
+        return prefix[right] - prefix[left - 1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
