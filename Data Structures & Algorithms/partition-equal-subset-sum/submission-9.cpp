a1bb5eae1d9a26ce18fe650912d0650d61e1c99a
Ans Christy <anschristykl@gmail.com>
2026-07-22 18:47:24 +0530

    Add: partition-equal-subset-sum - submission-9

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-9.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+
+        unordered_set<int> s;
+        s.insert(nums[nums.size() - 1]);
+        s.insert(0);
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            unordered_set<int> next = s;
+            for (auto& elem : s) {
+                if (s.count(target)) return true;
+                next.insert(nums[i] + elem);
+            }
+            
+            s = move(next);
+        }
+
+        return false;
+    }
+};
